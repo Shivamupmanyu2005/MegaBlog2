339da9fc2c08c2dd72cee0e81c39c8bc84923d08
Shivamupmanyu2005 <shivamupmanyu30@gmail.com>
2025-04-07 15:51:21 +0530

    Removed TinyMce as was causing editorial issues

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,57 +1,51 @@
 import React from "react";
-import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
+import { useQuill } from "react-quilljs";
+import "quill/dist/quill.snow.css";
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
     return (
-        <div className="w-full min-h-[500px] border border-gray-300 rounded-xl shadow-sm p-4 bg-white">
+        <div className="w-full min-h-[300px] border border-gray-300 rounded-xl shadow-sm p-4 bg-white">
             {label && (
                 <label className="inline-block mb-2 pl-1 font-medium">
                     {label}
                 </label>
             )}
+
             <Controller
                 name={name || "content"}
                 control={control}
-                render={({ field: { onChange, value } }) => (
-                    <Editor
-                        tinymceScriptSrc="/tinymce/tinymce.min.js" // Load TinyMCE from local file
-                        value={value}
-                        onEditorChange={onChange}
-                        initialValue={defaultValue}
-                        init={{
-                            height: 500,
-                            width: "100%",
-                            menubar: true,
-                            plugins: [
-                                "image",
-                                "advlist",
-                                "autolink",
-                                "lists",
-                                "link",
-                                "charmap",
-                                "preview",
-                                "anchor",
-                                "searchreplace",
-                                "visualblocks",
-                                "code",
-                                "fullscreen",
-                                "insertdatetime",
-                                "media",
-                                "table",
-                                "help",
-                                "wordcount"
+                defaultValue={defaultValue}
+                render={({ field: { onChange, value } }) => {
+                    const { quill, quillRef } = useQuill({
+                        theme: "snow",
+                        modules: {
+                            toolbar: [
+                                [{ header: [1, 2, false] }],
+                                ["bold", "italic", "underline"],
+                                ["image", "code-block"],
                             ],
-                            toolbar:
-                                "undo redo | blocks | image media | bold italic forecolor | " +
-                                "alignleft aligncenter alignright alignjustify | " +
-                                "bullist numlist outdent indent | table link | " +
-                                "code preview fullscreen | removeformat | help",
-                            content_style:
-                                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-                        }}
-                    />
-                )}
+                        },
+                    });
+
+                    // Update value on text change
+                    React.useEffect(() => {
+                        if (quill) {
+                            quill.on("text-change", () => {
+                                onChange(quill.root.innerHTML);
+                            });
+                        }
+                    }, [quill]);
+
+                    // Set default value
+                    React.useEffect(() => {
+                        if (quill && defaultValue) {
+                            quill.root.innerHTML = defaultValue;
+                        }
+                    }, [quill]);
+
+                    return <div ref={quillRef} />;
+                }}
             />
         </div>
     );
